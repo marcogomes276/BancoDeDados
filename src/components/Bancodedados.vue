a47b61ec45548ace013c916e12c70dae8171f881
Vai na Web <vainawebcpx@gmail.com>
2018-03-21 16:44:44 -0300

    tudo pronto git add .!

--- src/components/Bancodedados.vue
+++ src/components/Bancodedados.vue
@@ -3,21 +3,23 @@
 		<main class="conteiner">
 			<section class="inicio">
 				<img class="logo" src="../assets/Logo-sol.png">
-				<h1>Bem vindo!</h1>
-				<p>insira seu cep e os outros dados carregam automaticamente</p>
+				<h1 class="titulo">Bem vindo!</h1>
+				<p class="instrucion">insira seu cep e os outros dados carregam automaticamente</p>
 				<img class="seta" src="../assets/seta.png">
 			</section>
 			<section class="dados">
-				<p class="texto">Escreva seu Cep:</p>
-				<input type="text" v-model="cep" v-on:keyup.enter="getData()" class="dados-input">
+				<p class="texto">Cep:</p>
+				<input type="text" v-model="cep" v-on:keyup.enter="getData()" class="dados-input" placeholder="escreva seu CEP">
 				<p class="texto">Rua:</p>
 				<input type="text" v-model="rua" class="dados-input">
-				<p class="texto">Bairro</p>
+				<p class="texto">Bairro:</p>
 				<input type="text" v-model="bairro" class="dados-input">
 				<p class="texto">Cidade:</p>
 				<input type="text" v-model="cidade" class="dados-input">
 				<p class="texto">UF:</p>
 				<input type="text" v-model="uf" class="dados-input">
+				<button class="botao">cancel</button>
+				<button class="botao salvar">save</button>
 			</section>
 		</main>
 	</div>
@@ -69,6 +71,8 @@ export default{
 	justify-content: flex-start;
 	align-items: stretch;
 	background-color: #fff;
+	border-radius: 30px;
+	background-color: #b2b2b2;
 }
 .inicio{
 	width: 30%;
@@ -79,31 +83,64 @@ export default{
 	justify-content: center;
 	align-items: center;
 	flex-direction: column;
+	border-radius: 30px 0 0 30px; 
 }
 .logo{
-	width: 60%;
+	width: 30%;
+}
+.titulo{
+	margin: 30px;
+}
+.instrucion{
+	padding-left: 50px;
 }
 .seta{
-	width: 60%;
+	width: 30%;
 }
 .dados{
 	/*max-width: 70%;*/
 	min-width: 350px;
+	max-width: 350px;
+	padding: 10px;
+	padding-top: 100px;
 	margin: 0 auto;
 	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;	
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: flex-start;
+	align-items: flex-start;
+	align-content: flex-start;
+	background-color: #b2b2b2;	
 }
 .texto{
-	color: black;
-	font-size: 2em;
+	width: 80px;
+	color: #686767;
+	font-size: 1.5em;
+	display: inline;
+	margin-top: 40px;
 }
 .dados-input{
-	width: ;
-	height: ;
-	background-color: #03352c;
-	color: #fff;
-	border:none;	
+	width: 250px;
+	height: 5% ;
+	padding: 10px;
+	margin: 5px 0;
+	border-radius: 5px;	
+	display: inline;
+	background-color: white;
+	color: #737373;
+	border:none;
+	margin-top: 40px;
+}
+.botao{
+	width: 90px;
+	height: 40px;
+	border-radius: 10%;
+	border: none;
+	margin: 10px;
+	margin-left: 100px;
+}
+.salvar{
+	background-color: #52dee5;
+	margin: 10px 0 0 15px;
 }
 </style>
